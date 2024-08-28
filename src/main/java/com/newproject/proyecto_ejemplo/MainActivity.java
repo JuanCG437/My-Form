578b1b482f8fca46114218f45a71d171613e076a
JuanCG437 <tatumenes240@gmail.com>
2024-08-27 20:09:03 -0500

    se eliminaron algunas librerias
    Se eliminaron algunas librerías que no se estaban usando, además se editó el archivo MainActivity específicamente los elementos de EditText, para asociarlos con el id del xml donde había redundancia en su asociación

--- src/main/java/com/newproject/proyecto_ejemplo/MainActivity.java
+++ src/main/java/com/newproject/proyecto_ejemplo/MainActivity.java
@@ -5,17 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.newproject.proyecto_ejemplo.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nombre = (EditText) findViewById(R.id.editNombre);
-        apellidos = (EditText) findViewById(R.id.editApellido);
-        edad = (EditText) findViewById(R.id.editEdad);
-        correo = (EditText) findViewById(R.id.editCorreo);
-        aceptar = (Button) findViewById(R.id.btnAceptar);
+        nombre =  findViewById(R.id.editNombre);
+        apellidos =  findViewById(R.id.editApellido);
+        edad =  findViewById(R.id.editEdad);
+        correo =  findViewById(R.id.editCorreo);
+        aceptar =  findViewById(R.id.btnAceptar);
 
         aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 pasarDato.putExtra("surname", surname);
                 pasarDato.putExtra("age", age);
                 pasarDato.putExtra("email", email);
+
 
                 startActivity(pasarDato);
             }
