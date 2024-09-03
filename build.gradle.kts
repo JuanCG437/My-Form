fdd4b1e9c3877a75e4813b8c2455814411579469
JuanCG437 <tatumenes240@gmail.com>
2024-09-02 20:59:37 -0500

    Splash
    Se agregó animación mediante la pantalla/actividad splash utilizando las animaciones de Lottie.
    
    Además se configuró el archivo Manifest.xml para dar prioriodad a qué pantalla/activdad se visualizará primero

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.airbnb.android:lottie:6.5.1")
+
 }
---
+++ src/main/java/com/newproject/proyecto_ejemplo/Splash.java
@@ -0,0 +1,27 @@
+package com.newproject.proyecto_ejemplo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(Splash.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },3000);
+    }
+}
